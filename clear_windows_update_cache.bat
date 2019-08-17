2912a846f9b475ff6c8db5441d0205d72d2d50c6
hack.jacky <hackjackyer@users.noreply.github.com>
2019-08-17 15:47:59 +0800

    Create clear_windows_update_cache.bat
    清理更新缓存

---
+++ clear_windows_update_cache.bat
@@ -0,0 +1,14 @@
+@echo off
+:: by hackjackyer
+net stop wuauserv
+net stop cryptSvc
+net stop bits
+net stop msiserver
+ren C:\Windows\SoftwareDistribution SoftwareDistribution.old
+ren C:\Windows\System32\catroot2 catroot2.old
+net start wuauserv
+net start cryptSvc
+net start bits
+net start msiserver
+echo clear complete!
+pause
